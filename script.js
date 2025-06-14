a85cec4f066006a7b770d5a90bec41c881fc6328
Moran Danino <139646146+MoranDanino@users.noreply.github.com>
2025-06-14 17:21:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// כאשר הסרטון מסתיים, מציגים את התוכן
-document.getElementById('openingVideo').addEventListener('ended', function() {
-    document.getElementById('videoContainer').style.display = 'none';
-    document.getElementById('mainContent').style.display = 'block';
-});
+// // כאשר הסרטון מסתיים, מציגים את התוכן
+// document.getElementById('openingVideo').addEventListener('ended', function() {
+//     document.getElementById('videoContainer').style.display = 'none';
+//     document.getElementById('mainContent').style.display = 'block';
+// });
