e6b1647fb215f2e17533107306128101e6253e0e
Moran Danino <139646146+MoranDanino@users.noreply.github.com>
2025-06-14 16:49:33 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// כאשר הסרטון מסתיים, מציגים את התוכן
+document.getElementById('openingVideo').addEventListener('ended', function() {
+    document.getElementById('videoContainer').style.display = 'none';
+    document.getElementById('mainContent').style.display = 'block';
+});
